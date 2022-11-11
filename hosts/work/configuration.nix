170a11a916bd5ec66e135a55e0483cad6a951ec7
NAli <65361162+nishad-vkn@users.noreply.github.com>
2022-11-11 05:33:57 +0530

    Create configuration.nix

---
+++ hosts/work/configuration.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+  # Do not change
+  system.stateVersion = "22.11";
+
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../../modules/default.nix
+      ../../modules/desktop.nix
+      #../../modules/laptop.nix
+    ];
+
+  # Intel microcode
+  hardware.cpu.intel.updateMicrocode = true;
+
+  # Boot
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "work";
+  networking.useDHCP = false;
+
+  # ZFS
+  boot.supportedFilesystems = [ "zfs" ];
+  networking.hostId = "2020abab";
+  services.zfs.autoScrub.enable = true;
+  boot.zfs.requestEncryptionCredentials = true;
+
+  # Printers
+  services.printing.enable = true;
+  services.printing.drivers = [ pkgs.hplip ];
+  environment.systemPackages = with pkgs; [ cups-toshiba-estudio ];
+}
