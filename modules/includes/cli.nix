aaab4b62ba16e6a49a6c205d7066b836e73af9ed
NAli <65361162+nishad-vkn@users.noreply.github.com>
2022-11-11 05:43:30 +0530

    Create cli.nix

---
+++ modules/includes/cli.nix
@@ -0,0 +1,56 @@
+
+{ config, pkgs, ... }:
+
+{
+  # Enable fish.
+  programs.fish.enable = true;
+
+  # Enable libvirt
+  boot.kernelModules = [ "kvm-intel" ];
+  virtualisation.libvirtd.enable = true;
+
+  # Enable docker and podman
+  virtualisation.docker.enable = true;
+
+  # Root shell
+  users.extraUsers.root.shell = pkgs.fish;
+  users.extraUsers.user.shell = pkgs.fish;
+
+  # Start SSH agent
+  programs.ssh.startAgent = true;
+
+  # Install cli packages
+  environment.systemPackages = with pkgs; [
+    bc
+    file
+    curl
+    wget
+    jq
+    most
+    fzf
+    rsync
+    git
+    neovim
+    screen
+    tree
+    htop
+    nmap
+    nethogs
+    netcat-gnu
+    tcpdump
+    whois
+    nixops
+    lf
+    ncdu
+    asciinema
+    speedtest-cli
+    docker-compose
+    rclone
+    neofetch
+    unzip
+    ix
+    stress
+    bind
+    cifs-utils
+  ];
+}
