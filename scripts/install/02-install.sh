59569a018b686fc8ee1a35a2521d20fb561a17a5
NAli <65361162+nishad-vkn@users.noreply.github.com>
2022-11-11 05:50:04 +0530

    Create 02-install.sh

---
+++ scripts/install/02-install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+print () {
+    echo -e "\n\033[1m> $1\033[0m\n"
+}
+
+# Initialize Nixos configuration
+#nixos-generate-config --root /mnt
+rm -Rf /mnt/etc/nixos 2>/dev/null
+git clone https://github.com/nishad-vkn/gh-config /mnt/etc/nixos
+
+# Select host configuration
+print "Select host to setup :"
+WORKDIR="/mnt/etc/nixos/hosts/" ; cd $WORKDIR
+select HOST in $(ls);
+do
+    ln -s hosts/$HOST/configuration.nix ../
+
+    # Generate configuration-hardware.nix
+    print "Generate hardware configuration"
+    nixos-generate-config --root /mnt --show-hardware-config > /mnt/etc/nixos/hosts/$HOST/hardware-configuration.nix
+    break
+done
+
+
+# Set unstable channel
+nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+nix-channel --update
+
+# Install
+nixos-install
+
+# Finish
+echo -e "\e[32mAll OK"
