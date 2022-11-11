e265eb7fc661ce4d35a0746af863cdfa37de7e17
NAli <65361162+nishad-vkn@users.noreply.github.com>
2022-11-11 05:44:54 +0530

    Create desktop-packages.nix

---
+++ modules/includes/desktop-packages.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+  # Basic
+    firefox-bin
+    google-chrome
+    libreoffice
+    evince
+    xfce.thunar
+  # Music
+    pavucontrol
+    spotify
+    playerctl
+    audacity
+  # Video
+    mpv
+    zoom-us
+  # Image
+    feh
+    gnome-photos
+    imagemagick
+    kolourpaint
+  # Games
+    minecraft
+    #hedgewars # BUG
+    mindustry
+    steam
+  # Network
+    transmission
+    webtorrent_desktop
+    connman-gtk
+    weechat
+    #tiny
+  # Dev
+    pastel
+    vscodium
+  # Work
+    rdesktop
+    texlive.combined.scheme-small
+    vagrant
+    virt-manager
+    x2goclient
+    virtualbox
+    genymotion # android vm
+  # Term
+    termite
+    asciinema
+  # Tool
+    borgbackup
+    appimage-run
+  # GTK
+    variety
+    lxappearance
+    equilux-theme
+    paper-icon-theme
+  # Phone
+    adb-sync
+  ];
+}
